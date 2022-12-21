1bd0bcefbc49708f8b6413f679f377330f581539
seanm <saean@mail.com>
2022-12-21 16:45:04 +0000

    cognito auth

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_6fBiY00PG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2fqsp3c81fng5cl3qrn8e831g7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
