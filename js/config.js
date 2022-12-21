0bbb3dffc8b30bebe53ef68d798109e75a4ecd76
seanm <saean@mail.com>
2022-12-21 16:56:14 +0000

    auth2

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-2_6fBiY00PG', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2fqsp3c81fng5cl3qrn8e831g7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-2_fDOoDKGwq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7vrk6lr0mecpjtftdcpqlg473a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
